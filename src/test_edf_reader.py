51227ffbe3f0d87e72cf2c5a720c1dfa1594ca70
Hafsteinn Fannar Ragnarsson <haffi13@gmail.com>
2019-02-20 13:21:26 +0100

    :)
    Signed-off-by: Hafsteinn Fannar Ragnarsson <haffi13@gmail.com>

--- src/test_edf_reader.py
+++ src/test_edf_reader.py
@@ -14,19 +14,20 @@ import datetime
     h['number_of_signals'] 
 '''
 
-h = reader.load_edf_file('5.edf')
+h1 = reader.load_edf_file('5.edf')
+h2 = reader.load_edf_file('test_generator_2.edf')
 
 def test_data_format_version():
-    assert str(h['data_format_version']) == '0'
+    assert str(h1['data_format_version']) == '0'
 
 def test_local_patient_id():
-    assert str(h['local_patient_id']) == '5_MIN'
+    assert str(h1['local_patient_id']) == '5_MIN'
 
 def test_local_recording_id():
-    assert str(h['local_recording_id']) == 'Startdate 27.11.2018 Cortrium C3'
+    assert str(h1['local_recording_id']) == 'Startdate 27.11.2018 Cortrium C3'
 
 def test_start_time():
-    assert h['start_time'] == str(datetime.datetime(2018, 11, 27, 9, 26, 20))
+    assert h1['start_time'] == str(datetime.datetime(2018, 11, 27, 9, 26, 20))
 
 
 def test_bytes_in_header():
