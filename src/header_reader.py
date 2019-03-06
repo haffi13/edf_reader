f1b9748fb6cd0312a95914bf6946de0942e40108
Hafsteinn Fannar Ragnarsson <haffi13@gmail.com>
2019-03-06 15:03:47 +0100

    Can now plot one signal in a readable fashion. Pushing in this state to have a hardcoded copy in case our algorithming doesn't go that smooth.
    Signed-off-by: Hafsteinn Fannar Ragnarsson <haffi13@gmail.com>

--- src/header_reader.py
+++ src/header_reader.py
@@ -2,6 +2,10 @@ import datetime
 import os
 import re
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+import sys
+import math
 
 basestring = r'C:\ProjectResources'
 filename = '5.edf'  # The 5.edf file is lacking some of the properties an edf file can have, should use some other file for testing
@@ -111,6 +115,50 @@ def load_edf_file(edffile):
     reader = HeaderReader(edffile)
     reader.read_header()
     rectime, data_points, annotations = list(zip(*reader.records()))
+
+    i = 0
+    length = len(data_points)
+    sinewave = np.empty([0, 0])
+    print('------------------------------------------------------------------------------------------')
+    while i < length:
+        t = data_points[i][5]
+        i += 1
+        sinewave = np.append(sinewave, t)
+    print(len(sinewave))
+    print(type(sinewave))
+    sinemax = math.ceil(np.amax(sinewave))
+    sinemin = math.floor(np.amin(sinewave))
+    print('sinemax - ' + str(sinemax))
+    print('sinemin - ' + str(sinemin))
+    smin = sys.float_info.max
+    smax = sys.float_info.min
+    print(smin)
+    print(smax)
+    s = ''
+    for x in sinewave:
+        s += str(type(x))
+        s += ' - '
+    with open('type_result.txt', 'w') as wf:
+        wf.write(s)
+        print('file created!')
+    print('min = ' + str(smin) + '   max = ' + str(smax))
+
+    fig, ax = plt.subplots()
+    t = np.arange(0.0, 600.00, 0.005)  # (0, number of records, (number_of_records / number_of_records * sampling rate))
+    print(len(t))
+    plt.plot(t, sinewave)
+    plt.axis([0, 10, sinemin, sinemax])  # first 2 numbers are range that is visible at a time, last 2 are min/max for y axis
+    axpos = plt.axes([0.2, 0.1, 0.65, 0.03])  # position of the slider bar
+    spos = Slider(axpos, 'Pos', 0.1, 590)  # position, label, step size, length(max - visible range at a time)
+
+    def update(val):
+        pos = spos.val
+        ax.axis([pos, pos + 10, sinemin, sinemax])  # bottom_left_pos, pos+visible range at a time, min/max for y axis
+        fig.canvas.draw_idle()
+
+    spos.on_changed(update)
+    ax.grid(True)
+    plt.show()
 
     return reader.header  # The return values of this method cannot be changed without breaking tests.
     # 'b' prefix in front of string means it's a bytes literal
